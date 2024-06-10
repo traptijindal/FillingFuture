8c381b63e905aef254173a99fd125866231a8935
traptijindal <traptijindal5678@gmail.com>
2024-06-10 22:48:24 +0530

    about us page completed

--- src/Components/AboutHeader.jsx
+++ src/Components/AboutHeader.jsx
@@ -3,11 +3,12 @@ import hero from '/Hero_image.png'
 
 const AboutHeader = () => {
   return (
-  <div className='flex flex-col items-center justify-center mx-5 mt-10'>
+  <div className='flex flex-col items-center justify-center m-5  lg:mx-5 mt-10'>
       <div >
-      <h1 className='text-[#1D3C49] text-3xl font-bold mb-4'>About Us</h1>
-      <p className='mb-7 text-xl font-semibold'><span className='text-[#FFA500]'>Filling</span><span className='text-[#1D3C49]'>Future is a nonprofit organization bringing food to people around the world.</span> </p>
-      <img src={hero} alt="" className='w-full max-w-[650px] h-auto mx-auto rounded'/>
+      <h1 className='text-[#1D3C49] text-3xl md:text-4xl lg:text-5xl font-bold mb-7'>About Us</h1>
+      <p className='mb-5 md:mb-16 text-lg md:text-2xl lg:text-3xl  '><span className='text-[#FFA500]'>Filling</span><span className='text-[#1D3C49]'>Future is a nonprofit organization bringing food to people around the world.</span> </p>
+      {/* <img src={hero} alt="" className='w-full max-w-[650px] h-auto mx-auto rounded'/> */}
+      <img src={hero} alt="" className='w-[500px] md:w-[700px] lg:w-[900px]  mx-auto rounded'/>
     </div>
   </div>
   )
--- src/Components/About_work.jsx
+++ src/Components/About_work.jsx
@@ -22,14 +22,14 @@ const data_about =[
 
 const About_work = () => {
   return (
-    <div className='my-7 mx-4 md:mx-14 '>
-      <h1 className='text-[#1D3C49] text-3xl font-bold  '>How we work</h1>
+    <div className='my-7 mx-4 md:mx-14 flex flex-col  justify-center'>
+      <h1 className='text-[#1D3C49] text-3xl md:text-4xl font-bold  '>How we work</h1>
       <div className='flex flex-col md:flex-row justify-center items-center w-[1/3] '>
           {data_about.map((items,index)=>{
             return(
                 <div key={index}className='p-4 px-6 border-b border-[#d3cdcd] md:border-none my-2' >
-                     <img src={items.image} alt="" />
-                     <h1>{items.head}</h1>
+                     <img src={items.image} alt="" className='mx-auto'/>
+                     <h1 className='text-center text-[#1D3C49] font-bold text-lg mb-2'>{items.head}</h1>
                      <p>{items.text}</p>
                 </div>
             )
