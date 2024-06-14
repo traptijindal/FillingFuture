2cab84fef79fc23f22cb05f58c832b5572be4897
traptijindal <traptijindal5678@gmail.com>
2024-06-14 20:09:16 +0530

    fixed login and signup page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -3,8 +3,8 @@ import { FaFacebookF, FaGoogle, FaApple } from 'react-icons/fa'
 
 const Login = () => {
   return (
-    <div className='flex items-center justify-center min-h-screen mt-10'>
-      <div className='w-[350px] shadow-lg shadow-[rgb(136,134,134)]  p-6'>
+    <div className='flex items-center justify-center min-h-screen mt-5 md:mt-10 overflow-hidden'>
+      <div className='w-[300px] md:w-[350px] shadow-lg shadow-[rgb(136,134,134)]  p-6'>
         <h1 className='text-[#002844] font-bold text-2xl text-center mb-6'>Log in to <span className='text-[#FFA500]'>Filling</span>Future</h1>
         <div className=' mb-4 flex flex-row items-center justify-center text-center w-full'>
           <button className='w-[40%] py-3 mr-7 bg-[#3A579D] text-white rounded-sm flex flex-row items-center justify-center'>
@@ -25,8 +25,8 @@ const Login = () => {
         <p className='px-4'>or</p>
         <hr className='w-[39%] border-t-2 border-[#BAC8CE]' />
       </div>
-      <input type="text" placeholder="Email"    className='pr-20 pl-5 py-2 ml-2 border border-[#BAC8CE] rounded-sm my-4 text-start'/>
-      <input type="text" placeholder="Password" className='pr-20 pl-5 py-2 ml-2 border border-[#BAC8CE] rounded-sm text-start'/>
+      <input type="text" placeholder="Email"    className=' pr-10 md:pr-20 pl-5 py-2 ml-2 border border-[#BAC8CE] rounded-sm my-4 text-start'/>
+      <input type="text" placeholder="Password" className='pr-10 md:pr-20 pl-5 py-2 ml-2 border border-[#BAC8CE] rounded-sm text-start'/>
       <button className='w-[95%] px-6 py-2  bg-[#FFA500] text-white rounded-lg flex items-center justify-center mx-auto my-4'>
         Login with email
        </button>
