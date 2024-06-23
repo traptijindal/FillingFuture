57e3542456f090e24b81c5c90001cc1c0a8e9d52
traptijindal <traptijindal5678@gmail.com>
2024-06-23 17:31:01 +0530

    updates

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FaFacebookF, FaGoogle, FaApple } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -7,8 +7,8 @@ const Signup = () => {
     <div className='flex items-center justify-center min-h-screen mt-5 md:mt-10 overflow-hidden'>
       <div className='w-[300px] md:w-[350px] shadow-lg shadow-[rgb(136,134,134)]  p-6'>
         <h1 className='text-[#002844] font-bold text-xl md:text-2xl text-center mb-6'>Sign up for <span className='text-[#FFA500]'>Filling</span>Future</h1>
-        <input type="text" placeholder="Email"    className='pr-10 md:pr-20 pl-5 py-2 ml-2 border border-[#BAC8CE] rounded-sm my-4 text-start'/>
-      <input type="text" placeholder="Password" className='pr-10 md:pr-20 pl-5 py-2 ml-2 border border-[#BAC8CE] rounded-sm   mb-4 text-start'/>
+        <input type="text" placeholder="Email"    className='pr-5 md:pr-20 pl-5 py-2 ml-2 border border-[#BAC8CE] rounded-sm my-4 text-start'/>
+      <input type="text" placeholder="Password" className='pr-5 md:pr-20 pl-5 py-2 ml-2 border border-[#BAC8CE] rounded-sm   mb-4 text-start'/>
         <button className='w-[95%] px-6 py-2 bg-[#FFA500] text-white rounded-lg flex items-center justify-center mx-auto font-semibold'>
            Create account
         </button>
